d637363818343bb39324f58553e52671a8f437fd
zoe <zoeedge0703@gmail.com>
2023-08-27 17:31:31 -0500

    made the User model and associated thoughts and friends model but need to make the Friend and Thought model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose')
+const emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+
+const userSchema = new Schema(
+    {
+        username: { 
+            type: String, 
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: emailRegEx
+        },
+        // Need to make this model still
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thoughts'
+            }
+        ],
+        // Need to make this model still
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'friends'
+            }
+        ]
+        
+
+    },
+
+
+)
