b3e422f8027afb73f218b54018b8ab69168807d6
zoe <zoeedge0703@gmail.com>
2023-08-27 16:12:10 -0500

    Setting up the server and making all the connections to express, etc. Issue #1 is partially finished

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const db = require('./config/connection');
+const routes = require('./routes');
+// const { MongoClient } = require('mongodb');
+
+const cwd = process.cwd();
+
+
+const PORT = 3001;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// once database is open then start listening for requests 
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server for social-network is running on port ${PORT}!`);
+    });
+});
