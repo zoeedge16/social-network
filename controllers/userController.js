24818aa381ecd5ae34003acf3374f4edc9af9f1e
zoe <zoeedge0703@gmail.com>
2023-08-30 19:53:49 -0500

    created add friend and delete friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,5 +53,43 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
+    },
+
+    // add friend 
+
+    async addFriend(req,res) {
+       try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId} },
+            { runValidators: true, new: true }
+        );
+        if (!user) {
+            return res.status(404).json({ message: 'Friend added, user not found' })
+        }
+        res.json({ message: 'Friend added' })
+       } catch(err) {
+        res.status(500).json(err)
+       }
+    },
+
+    // delete friend
+
+    async deleteFriend(req,res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: {friends: req.params.friendId} },
+                { new: true }
+            );
+
+        if (!user) {
+            return res.status(404).json( { message: 'User not found' } )
+        }
+
+        res.json( { message: 'friend deleted' } )
+        } catch (err) {
+            res.statue(500).json(err)
+        }
     }
 };
